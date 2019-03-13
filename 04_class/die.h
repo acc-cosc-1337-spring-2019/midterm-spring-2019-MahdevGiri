c3926e1bb9ffd6d3fec47228d1eb709337b95be7
mahesh giri <mahesh.irig77@gmail.com>
2019-03-12 23:21:38 -0500

    04_class exercise done

--- 04_class/die.h
+++ 04_class/die.h
@@ -1,3 +1,4 @@
+#include<iostream>
 /*
 Create a Die class interface to model a game with 6 sides 
 and values 1,2,3,4,5, and 6.
@@ -6,3 +7,13 @@ public function roll that returns an int
 
 private int sides with a value of 6
 */
+class Die
+{
+public:
+	void roll();  // assigns random roll_value
+	int rolled_value()const;  // returns roll_value
+
+private:
+	int roll_value;
+	int sides = 6;
+};
--- 04_class/main.cpp
+++ 04_class/main.cpp
@@ -1,3 +1,6 @@
+#include"die.h"
+#include<iostream>
+
 //Write include statement
 /*
 Program runs until user opts out.
@@ -8,5 +11,19 @@ For each loop Roll die and display output
 
 int main() 
 {
+	Die die;
+	char userChoice;
+
+	do 
+	{
+		std::cout << "Rolling the die. \n";
+		die.roll();
+		std::cout <<"Roll Value: "<< die.rolled_value()<<"\n";
+		std::cout << "Do you want to roll again? " << "\n";
+		std::cout << "Enter 'y' to try again or enter any other key to exit" << "\n";
+		std::cin >> userChoice;
+
+	} while (userChoice == 'y' || userChoice == 'Y');
+	
 	return 0;
 }

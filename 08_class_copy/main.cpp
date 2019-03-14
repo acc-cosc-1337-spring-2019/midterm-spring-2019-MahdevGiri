f74a0303906b7851da74efe0f3622e783a5fb1b6
mahesh giri <mahesh.irig77@gmail.com>
2019-03-14 12:18:04 -0500

    class copy

--- 08_class_copy/main.cpp
+++ 08_class_copy/main.cpp
@@ -4,9 +4,34 @@ Explain what happens with comments in your code.
 Create a reference to a Die, explain what happens with comments in your code.  
 Create test cases in 08_class_copy_test.
 */
+#include"die.h"
 
-int main() 
+int main()
 {
+	Die d1;
+	d1.roll();
+	std::cout << "d1 value: " << d1.rolled_value() << "\n";
 	
+	Die d1Copy = d1;
+	std::cout << "d1Copy value: " << d1Copy.rolled_value() << "\n";
+	// This will copy the d1 object in d1Copy object
+	// The change made to d1 object after the copy statement will not be updated to the d1Copy object
+	// because it is only copy , and it doesnot point to actual memory address of d1 object
+	
+	Die& d1Ref = d1;
+	std::cout << "d1Ref value: " << d1Ref.rolled_value() << "\n\n";
+	// The d1Ref object will point to actual memory address of d1 object
+	// So the every change made to d1 object will be automatically updated to d1Ref object
+	
+
+	for (int i = 0; i < 10; i++)
+	{
+	
+	d1.roll();
+	std::cout <<"d1 value: "<< d1.rolled_value() << "\n";
+    std::cout <<"d1Copy value: "<< d1Copy.rolled_value() << "\n";    // change will not be updated
+	std::cout << "d1Ref value: " << d1Ref.rolled_value() << "\n\n";  // change will be updated
+	}
+
 	return 0;
 }
--- 08_class_copy_test/copy_test.cpp
+++ 08_class_copy_test/copy_test.cpp
@@ -1,5 +1,26 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include"die.h"
+using namespace std;
 
 //Create a test case that shows that a copy of Die are not the same
 //objects in memory
+
+TEST_CASE("test case that shows that a copy of Die are not the same objects in memory")
+{
+	Die d1;
+	d1.roll();
+
+	Die d1Copy = d1;
+
+    for (int i = 0; i < 5; i++)
+	{
+
+		d1.roll(); 
+		
+	}
+	
+	REQUIRE(d1.rolled_value()!= d1Copy.rolled_value());
+
+
+}

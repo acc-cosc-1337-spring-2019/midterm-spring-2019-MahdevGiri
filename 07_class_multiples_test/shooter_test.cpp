ee971067578559a50c995c4f2045a0b86073f00c
mahesh giri <mahesh.irig77@gmail.com>
2019-03-14 10:24:52 -0500

    some changes

--- 07_class_multiples_test/shooter_test.cpp
+++ 07_class_multiples_test/shooter_test.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include"shooter.h"
 
 /*
 Test that shooter returns a Roll and verify that the roll result has one 
@@ -9,3 +10,26 @@ Craps
 Point
 
 */
+
+
+TEST_CASE("Test case for roll result")
+{
+	Die d1;
+	Die d2;
+	Shooter shootObj;
+
+	for (int i = 0; i < 20; i++)
+	{
+		Roll rollObj = shootObj.shoot(d1, d2); // shoot returns the Roll 
+		bool check = false;
+		std::string result = rollObj.result();
+
+		if (result == "Craps" || result == "Natural" || result == "Points")
+		{
+			check = true;
+
+		}
+
+		REQUIRE(check == true);
+	}
+}
